4189502bb37386ab0ed982bdb1a0472a2e7b5180
Xyphuz <hansen0715.2@gmail.com>
2022-05-11 20:14:45 +0800

    test: update test_version

--- tests/test_voice_presentation_control.py
+++ tests/test_voice_presentation_control.py
@@ -1,5 +1,12 @@
-from voice_presentation_control import __version__
+from typer.testing import CliRunner
+from voice_presentation_control import cli, __app_name__, __version__
+
+
+runner = CliRunner()
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    result = runner.invoke(cli.app, ["--version"])
+
+    assert result.exit_code == 0
+    assert f"{__app_name__} {__version__}\n" in result.stdout
