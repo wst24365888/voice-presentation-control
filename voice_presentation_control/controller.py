311d98e58deee8597ba93b35c1bd169d72b455f9
hahaherher <hong625017@gmail.com>
2022-06-02 00:40:39 +0800

    feat: adjust volume before recognizing, chore: revise 'save_frames_to_wav'

--- voice_presentation_control/controller.py
+++ voice_presentation_control/controller.py
@@ -6,6 +6,7 @@ from glob import glob
 from multiprocessing import cpu_count
 from queue import Queue
 from typing import List
+import numpy as np
 
 import pyaudio
 
@@ -45,13 +46,32 @@ class Controller:
 
         self.executor = ThreadPoolExecutor(max_workers=cpu_count())
 
+
+
     def put_queue(self, _queue: Queue, item: bytes) -> None:
         if _queue.full():
             _queue.get()
         _queue.put(item)
 
+    def adjust_volume(self, record_frames: list, std_vol: int) -> None:
+        # find max volume in frames
+        amp = 0
+        for f in record_frames:
+            data_chunk = array("h", f)
+            amp = max(amp,max(data_chunk))
+        volume_scaler = (std_vol/amp)
+
+        # adjust volume
+        for i in range(0, len(record_frames)):
+            data_chunk = array("h", record_frames[i])
+            for j in range(0, len(data_chunk)):
+                data_chunk[j] = int(float(data_chunk[j]) * volume_scaler)
+            record_frames[i] = array.tobytes(data_chunk)
+
+
     def start(self) -> None:
         self.stream = self.mic.start(self.chunk, self.rate)
+        self.standard_volume = 10000
 
         while True:
             data = self.stream.read(self.chunk)
@@ -61,6 +81,7 @@ class Controller:
             max_vol = max(data_chunk)
 
             if max_vol >= self.threshold:
+
                 # print("recording triggered")
 
                 silent_flag = 0
@@ -87,11 +108,18 @@ class Controller:
                         # sliding window
                         if progress_counter % int((self.rate / self.chunk) * self.chunk_sliding_step) == 0:
                             record_frames = list(self.record_frame_q.queue)
+                            # adjust volume before getting recognizer result
+                            self.adjust_volume(record_frames, self.standard_volume)
+
                             self.executor.submit(self.get_recognizer_result, record_frames)
 
                 if not self.record_frame_q.full():
                     # for very short record
                     record_frames = list(self.record_frame_q.queue)
+                    # adjust volume before getting recognizer result
+                    #self.save_frames_to_wav(record_frames, 0)
+                    self.adjust_volume(record_frames, self.standard_volume)
+                    #self.save_frames_to_wav(record_frames, 1)
                     self.executor.submit(self.get_recognizer_result, record_frames)
 
                 record_frame_dq: deque = self.record_frame_q.queue
@@ -108,9 +136,11 @@ class Controller:
             msg = self.action_matcher.match(result)
             print(f"({msg})", flush=True)
 
-    def save_frames_to_wav(self, frames: List[bytes]) -> None:
-        num_files = len(glob("voice_presentation_control/wav_files/*.wav"))
-        wavefile = wave.open(f"voice_presentation_control/wav_files/test_save_{num_files}.wav", "wb")
+    def save_frames_to_wav(self, frames: List[bytes], num_files : int) -> None:
+        #num_files = len(glob("voice_presentation_control/wav_files/*.wav"))
+        #wavefile = wave.open(f"voice_presentation_control/wav_files/test_save_{num_files}.wav", "wb")
+        wavefile = wave.open(f"./wav_files/test_save_{num_files}.wav", "wb")
+
         wavefile.setnchannels(1)
         wavefile.setsampwidth(audio.get_sample_size(pyaudio.paInt16))
         wavefile.setframerate(self.rate)
